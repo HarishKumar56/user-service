f309bf481967c61ae33811ba458be5ea36b56fcb
Harish_Kumar2 <harish_kumar2@epam.com>
2021-09-16 19:30:59 +0530

    Refactored

--- src/main/java/com/epam/rd/entity/User.java
+++ src/main/java/com/epam/rd/entity/User.java
@@ -5,12 +5,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @Column(name = "user_name")
     private String userName;
+    @Column(name = "password")
     private String password;
+    @Column(name = "name")
     private String name;
 }
